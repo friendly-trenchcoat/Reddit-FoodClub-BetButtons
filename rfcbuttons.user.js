7e796cb17ec74e8f78b34402ad23325862f775c2
friendly-trenchcoat <tippywigwom@yahoo.com>
2016-04-13 09:38:32 -0500

    added check for invalid pirate names

--- rfcbuttons.user.js
+++ rfcbuttons.user.js
@@ -11,30 +11,30 @@
 
 /*
 HOT TIPS!
-I can't figure out a way to make it not switch you to the tab the button opens! 
+I can't figure out a way to make it not switch you to the tab the button opens!
 For a less annoying experience:
  - Best used in single window
  - CTRL+SHIFT+TAB   go to tab to the left
  - CTRL+TAB         go to tab to the right
- 
+
 ALSO: Don't like how the bet amount button scrolls with you? Scroll down and read how to change it.
 
-ALSO ALSO: There's nothing to stop you from submitting bets from past days. Try not to do this. 
+ALSO ALSO: There's nothing to stop you from submitting bets from past days. Try not to do this.
 */
 
 if(document.URL.indexOf("comments") != -1) {
-    // BET AMOUNT BUTTON        who knows if this works :D 
+    // BET AMOUNT BUTTON        who knows if this works :D
     var today = new Date();
     var offset = Math.floor(today.getTimezoneOffset() / 60) - 7;  // difference between UTC and local time in hours, - 8
     today.setHours(today.getHours() + offset);
 
     var betAmt = 0;
     var title = "click to set";
-    if(typeof GM_getValue("iBetAmt") !== 'undefined'){ 
+    if(typeof GM_getValue("iBetAmt") !== 'undefined'){
         var startDate = new Date(GM_getValue("startDate"));  // make back into a Date object
         var justToday = today;
         var days = Math.floor( (justToday.setHours(0,0,0,0) - startDate.setHours(0,0,0,0)) /(1000*60*60*24)  ); // number of days between start day and today
-        betAmt = GM_getValue("iBetAmt") + (days*2); // final bet amount 
+        betAmt = GM_getValue("iBetAmt") + (days*2); // final bet amount
         title = betAmt;
 
         // just testing stuff
@@ -45,11 +45,11 @@ if(document.URL.indexOf("comments") != -1) {
         console.log("NEW BET: "+betAmt);
 
     }
-    var betAmtButton = $('<button/>', {  
+    var betAmtButton = $('<button/>', {
         text: "bet amount: "+title,
         id: "betAmtButton",
-        click: function () { 
-            var iBetAmt = prompt("Bet amount: ", betAmt); 
+        click: function () {
+            var iBetAmt = prompt("Bet amount: ", betAmt);
             GM_setValue("iBetAmt", Number(iBetAmt));
             GM_setValue("startDate", today);
             betAmt = Number(iBetAmt);
@@ -62,19 +62,23 @@ if(document.URL.indexOf("comments") != -1) {
 
     // COMMENT OUT THIS LINE TO MAKE BET AMOUNT BUTTON NOT SCROLL WITH YOU********************************
     // OR change number values of "top" and "left" to reposition
-    $('#betAmtButton').css({"position":"fixed", "top":"20px", "left":"0px", "z-index":"100"}); 
+    $('#betAmtButton').css({"position":"fixed", "top":"20px", "left":"0px", "z-index":"100"});
 
 
 
     // BET BUTTONS
-    var pirates = ['Dan', 'Sproggie', 'Orvinn', 'Lucky', 'Edmund', 'Peg Leg', 'Bonnie', 'Puffo', 'Stuff', 'Squire', 'Crossblades', 'Stripey', 'Ned', 'Fairfax', 'Gooblah', 'Franchisco', 'Federismo', 'Blackbeard', 'Buck', 'Tailhook'];
+    var pirates = ['', 'Dan', 'Sproggie', 'Orvinn', 'Lucky', 'Edmund', 'Peg Leg', 'Bonnie', 'Puffo', 'Stuff', 'Squire', 'Crossblades', 'Stripey', 'Ned', 'Fairfax', 'Gooblah', 'Franchisco', 'Federismo', 'Blackbeard', 'Buck', 'Tailhook'];
     var pirateIDs = [0,0,0,0,0]; // five arenas
     $("tbody").children().each(function(k,v) {
-        if ($(v).children().length == 7){ //if it's actually a bet table        
-            for (var i=1; i<6; i++){ //for each column 
-                var arena = $(v).children().eq(i);//.css("background-color", "#ffc");  // delete ";//" if colorful tables gets you off
-                pirateIDs[i-1] = pirates.indexOf(arena.text())+1;
-                if (pirateIDs[i-1] == 0) {
+        if ($(v).children().length == 7){ //if it's actually a bet table
+            for (var i=1; i<6; i++){ //for each column
+                var arena = $(v).children().eq(i);//.css("background-color", "#ffc");  // HEY YOU THERE, delete ";//" after "eq(i)" if colorful tables gets you off*********************
+                if (pirates.indexOf(arena.text()) < 0){ // don't draw a button if the pirate is not in the list
+                    $(v).append('Invalid');
+                    return;
+                }
+                pirateIDs[i-1] = pirates.indexOf(arena.text());
+                if (pirateIDs[i-1] === 0) {
                     pirateIDs[i-1] = '<input type="hidden" name="winner'+i+'" value="">';
                 }
                 else {
@@ -95,6 +99,6 @@ else if(document.URL.indexOf("current") != -1) {
     if ( $("tr[bgcolor='white'").first().parent().children().length == 13) {
         $("tr[bgcolor='white'").each(function(k,v) {
             $(v).css("background-color", "#ffd");
-        });   
+        });
     }
 }
